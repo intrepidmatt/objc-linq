269997244c36321097ac2aed605bcaf00ecf8b94
Matt Bridges <matt@intrepid.io>
2012-11-10 13:47:20 -0500

    Initial Commit

---
+++ linqtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  linqtest
+//
+//  Created by Matt Bridges on 11/10/12.
+//  Copyright (c) 2012 Matt Bridges. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ linqtest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  linqtest
+//
+//  Created by Matt Bridges on 11/10/12.
+//  Copyright (c) 2012 Matt Bridges. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
